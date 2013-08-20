fc96690f34f4d2c57fc5f6eeab667d058ae6dbb5
Canan GİRGİN <canan.girgin@tubitak.gov.tr>
2013-08-20 16:58:01 +0300

    initial

---
+++ src/test/java/TaggerTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: a
+ * Date: 8/20/13
+ * Time: 4:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaggerTest {
+}
